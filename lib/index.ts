37149955f462c8f29707d65dd48d2940a682c7ee
Niklas Eicker <nikle1@gmx.de>
2019-10-22 20:05:19 +0200

    start game class

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,11 @@
-export function hi(): string {
-    return "Hey Ho";
+export class TakGame {
+    private _boardSize: number;
+    constructor(state: string) {
+        //ToDo: get size from passed state
+        this._boardSize = 5;
+    }
+
+    get boardSize(): number {
+        return this._boardSize;
+    }
 }
