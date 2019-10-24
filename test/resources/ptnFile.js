2fdaa35e48de32d392b3dd79a43e021e42a9292c
Niklas Eicker <git@nikl.me>
2019-10-24 22:43:29 +0200

    test PTN file parser

---
+++ test/resources/ptnFile.js
@@ -0,0 +1,22 @@
+let ptnFile = `[Site "PlayTak.com"]
+[Event "Online Play"]
+[Date "2018.10.28"]
+[Time "16:10:44"]
+[Player1 "NohatCoder"]
+[Player2 "fwwwwibib"]
+[Clock "10:0 +20"]
+[Result "R-0"]
+[Size "6"]
+
+1. a6 f6
+2. d4 c4
+3. d3 c3
+4. d5 c5
+5. d2 Ce4
+6. c2 e3
+7. e2 b2
+8. Cb3 1e4<1
+9. 1d3<1 Sd1
+10. a3 1d1+1`
+
+exports.ptnFile = ptnFile
--- test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var expect = require('chai').expect;
-var index = require('../dist/index.js');
-describe('hi function test', () => {
-    it('should return "Hey Ho"', () => {
-        var result = index.hi();
-        expect(result).to.equal('Hey Ho');
-    });
-});
