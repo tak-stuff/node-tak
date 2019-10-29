7c7a5f7d633c2d9ed9b0439c3f4ed54211767aca
Niklas Eicker <git@nikl.me>
2019-10-29 14:14:17 +0100

    test game state taking player names and result/status from PTN file

--- test/gameStateTests.js
+++ test/gameStateTests.js
@@ -1,10 +1,19 @@
 "use strict";
 const expect = require("chai").expect;
 const GameState = require("../dist/gameState")
+const ptnFile = require('./resources/ptnFile')
 
 describe("GameState", () => {
   it("should assume a board size of 5", () => {
     var state = new GameState();
     expect(state.size).to.equal(5);
+  });
+  it("should read data from PTN file", () => {
+    var state = GameState.fromPtnFile(ptnFile.simplePtnFile);
+    expect(state.size).to.equal(6);
+    expect(state.playerOne).to.equal("NohatCoder");
+    expect(state.playerTwo).to.equal("fwwwwibib");
+    expect(state.isGameOver).to.be.true;
+    expect(state.result).to.equal("R-0");
   });
 });
